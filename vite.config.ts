1086b826b615cf622bb8f2fe5bfc663ed73450e9
Hououin Kyouma <inwazzin@gmail.com>
2023-07-11 00:00:36 +0200

    remove unused scripts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import { resolve } from "path";
 
 export default defineConfig({
   build: {
+    minify: false,
     outDir: "build",
     emptyOutDir: false,
     lib: {
       name: "obsidian-js",
-      formats: ["es"],
+      formats: ["cjs"],
       entry: resolve(__dirname, "src/main.ts"),
       fileName: "main",
     },
     rollupOptions: {
       external: ["obsidian"],
       output: {
+        entryFileNames: "main.js",
         globals: {
           obsidian: "obsidian",
         },
